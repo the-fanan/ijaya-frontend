47b1338c2bb71c0de806e2bc32fe9a2338e2964a
Fanan Dala <fanan.dala@yahoo.com>
2019-10-20 00:18:57 +0100

    feat(artistes): added social cards

--- pages/artistes/_slug/index.vue
+++ pages/artistes/_slug/index.vue
@@ -8,23 +8,56 @@
 import { mapMutations, mapState } from 'vuex'
 
 export default {
+  data() {
+    return {
+      
+    }
+  },
   mounted () {
     this.$nextTick(() => {
       this.$nuxt.$loading.start()
-
       setTimeout(() => this.$nuxt.$loading.finish(), 1000)
     })
   },
   computed: {
     ...mapState({artists: state => state.artists}),
+    title() {
+      let slug = this.$route.params.slug
+      let slugParts = slug.split("-")
+      let title = ""
+      for (let i = 0; i < slugParts.length; i++) {
+        if (i == 0) {
+          title = slugParts[i].charAt(0).toUpperCase() + slugParts[i].slice(1).toLowerCase() + " "
+        } else {
+          title = title + " " + slugParts[i].charAt(0).toUpperCase() + slugParts[i].slice(1).toLowerCase()
+        }
+      }
+      title = title + " - Ijaya Artiste"
+      return title
+    },
   },
   methods: {
     ...mapMutations({incrementArtist: 'artists/increment'}),
   },
   components: {
   },
-  head: {
-    title: 'Ijaya Artistes'
+  head() {
+    return {
+      title: this.title,
+      meta: [
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:site', content: '@Dremdo' },
+        { name: 'twitter:creator', content: '@IjayaManagement' },
+        { name: 'twitter:title', content: 'Dremdo - Ijaya Artist' },
+        { name: 'twitter:description', content: 'View Dremdo\'s story on Ijaya' },
+        { name: 'twitter:image', content: 'https://image.com/image.png' },
+        { property: 'og:url', content: process.env.BASE_URL + this.$route.path },
+        { property: 'og:type', content: 'article' },
+        { property: 'og:title', content: 'Dremdo - Ijaya Artist' },
+        { property: 'og:description', content: 'View Dremdo\'s story on Ijaya' },
+        { property: 'og:image', content: 'https://image.com/image.png' },
+      ],
+    }
   }
 }
 </script>
