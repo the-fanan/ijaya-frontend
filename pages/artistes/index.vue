e77d3cf57d5ebff45eb64a75de5f3eab88cfd7ae
Fanan Dala <fanan.dala@yahoo.com>
2019-10-19 05:07:39 +0100

    added static pages

---
+++ pages/artistes/index.vue
@@ -0,0 +1,88 @@
+<template>
+  <section class="container">
+    <div>
+      <app-logo/>
+      <h1 class="title">
+        SSR Artists
+      </h1>
+      <h2 class="subtitle">
+        Frontend for Ijaya Management
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green">Documentation</a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey">GitHub</a>
+          <nuxt-link to="/">home</nuxt-link>
+          
+          <b-button @click="incrementMusic" variant="outline-info" class="m-1 foo-hover" size="lg">
+            Foo <i class="fab fa-github">{{ music.counter }}</i>
+          </b-button>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+import axios from 'axios'
+import { mapMutations, mapState } from 'vuex'
+
+export default {
+  mounted () {
+    this.$nextTick(() => {
+      this.$nuxt.$loading.start()
+
+      setTimeout(() => this.$nuxt.$loading.finish(), 1000)
+    })
+  },
+  computed: {
+    ...mapState({music: state => state.music}),
+  },
+  methods: {
+    ...mapMutations({incrementMusic: 'music/increment'}),
+  },
+  components: {
+    AppLogo
+  },
+  head: {
+    title: 'ssr foo'
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
+
