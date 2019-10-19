ae4219026627cc6b0a1dd9a27efb857f1c81ca0a
Fanan Dala <fanan.dala@yahoo.com>
2019-10-19 05:07:29 +0100

    feat(home): added parallax section

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,20 @@
 <template>
   <b-container :fluid="true">
     <b-row>
-      <b-col class="hero">
+      <b-col class="hero" id="hero">
         <transition name="hero-images-transition"
         enter-active-class="fade-in-animation"
 				leave-active-class="fade-out-animation"
         appear 
         v-on:after-appear="changeHeroImage"
         v-on:after-enter="changeHeroImage"
+        mode="in-out"
         >
           <img class="responsive" :src="heroImages[currentHeroImage].src" v-bind:key="heroImages[currentHeroImage].name"/>
         </transition>
         <b-row class="hero-scroll-down-holder">
           <b-col class="d-flex justify-content-center">
-            <span class="text-white text-center">
+            <span class="text-white text-center clicker" @click="scrollFromHero">
               <p class="text-center">Scroll Down</p>
               <i class="hover fa fa-chevron-down"></i>
             </span>
@@ -24,7 +25,7 @@
 
     <b-row>
       <b-col>
-        <parallax>
+        <parallax :speedFactor="0.3" :sectionHeight="60" sectionClass="artistes-parallax" containerClass="artistes-parallax-image">
           <img src="~/assets/images/artistes/artist-band.jpg" alt="artistes">
         </parallax>
       </b-col>
@@ -57,7 +58,13 @@ export default {
     changeHeroImage()
     {
       setTimeout(() => {this.currentHeroImage = (this.currentHeroImage + 1) % this.heroImages.length;}, 3000)
-    }
+    },
+    scrollFromHero()
+    {
+      let hero = document.getElementById("hero");
+      let scrollTo = hero.offsetTop + hero.scrollHeight
+      window.scrollTo(0, scrollTo)
+    },
   },
   components: {
     Parallax
