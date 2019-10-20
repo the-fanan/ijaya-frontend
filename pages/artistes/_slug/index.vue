a98926a7302f672e8fb82a100458e8166b8ecd36
Fanan Dala <fanan.dala@yahoo.com>
2019-10-20 09:55:51 +0100

    feat(artiste): added white and red name partition

--- pages/artistes/_slug/index.vue
+++ pages/artistes/_slug/index.vue
@@ -1,6 +1,25 @@
 <template>
 	<b-container :fluid="true">
-		
+		<b-row class="single-artiste-row">
+      <b-col md="6" cols="12" class="image-col">
+        <div class="image-holder">
+          <img class="artiste-image" src="~/assets/images/music/nagato-pain.jpg"/>
+          <div class="artiste-summary">
+            <h1 class="artiste-name text-center">{{ artisteName.whiteText }} <span class="red-text">{{ artisteName.redText }}</span></h1>
+            <ul class="artiste-social social-links d-flex h-100 align-items-center justify-content-center">
+              <li><a href="#"><i class="fab fa-twitter fa-lg"></i></a></li>
+              <li><a href="#"><i class="fab fa-instagram fa-lg"></i></a></li>
+              <li><a href="#"><i class="fab fa-youtube fa-lg"></i></a></li>
+              <li><a href="#"><i class="fab fa-spotify fa-lg"></i></a></li>
+              <li><a href="#"><i class="fab fa-soundcloud fa-lg"></i></a></li>
+              <li><a href="#"><i class="fab fa-apple fa-lg"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </b-col>
+      <b-col md="6" cols="12">
+      </b-col>
+		</b-row>
 	</b-container>
 </template>
 
@@ -35,6 +54,31 @@ export default {
       title = title + " - Ijaya Artiste"
       return title
     },
+    artisteName() {
+      let slug = this.$route.params.slug
+      let slugParts = slug.split("-")
+      let whiteText = ""
+      let redText = ""
+
+      for (let i = 0; i < Math.round((2/3) * slugParts.length); i++) {
+        if (i == 0) {
+          whiteText = slugParts[i].charAt(0).toUpperCase() + slugParts[i].slice(1).toLowerCase() + " "
+        } else {
+          whiteText = whiteText + " " + slugParts[i].charAt(0).toUpperCase() + slugParts[i].slice(1).toLowerCase()
+        }
+      }
+
+      for (let i = Math.round((2/3) * slugParts.length); i < slugParts.length; i++) {
+        if (i == 0) {
+          redText = slugParts[i].charAt(0).toUpperCase() + slugParts[i].slice(1).toLowerCase() + " "
+        } else {
+          redText = redText + " " + slugParts[i].charAt(0).toUpperCase() + slugParts[i].slice(1).toLowerCase()
+        }
+      }
+      let fullName = whiteText + " " + redText
+      
+      return {whiteText, redText, fullName}
+    }
   },
   methods: {
     ...mapMutations({incrementArtist: 'artists/increment'}),
@@ -48,12 +92,12 @@ export default {
         { name: 'twitter:card', content: 'summary_large_image' },
         { name: 'twitter:site', content: '@Dremdo' },
         { name: 'twitter:creator', content: '@IjayaManagement' },
-        { name: 'twitter:title', content: 'Dremdo - Ijaya Artist' },
+        { name: 'twitter:title', content: this.title },
         { name: 'twitter:description', content: 'View Dremdo\'s story on Ijaya' },
         { name: 'twitter:image', content: 'https://image.com/image.png' },
         { property: 'og:url', content: process.env.BASE_URL + this.$route.path },
         { property: 'og:type', content: 'article' },
-        { property: 'og:title', content: 'Dremdo - Ijaya Artist' },
+        { property: 'og:title', content: this.title },
         { property: 'og:description', content: 'View Dremdo\'s story on Ijaya' },
         { property: 'og:image', content: 'https://image.com/image.png' },
       ],
